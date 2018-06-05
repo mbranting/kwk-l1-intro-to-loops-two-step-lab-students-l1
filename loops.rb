c63b10a72ce5b0b8b261746b3bf062ab461c5c11
mbranting <mckenna.branting@gmail.com>
2018-06-05 20:49:11 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -6,22 +6,22 @@
 #   break
 # end
 
-def dance
-  moves_completed = 0
+# def dance
+#   moves_completed = 0
   
-  loop do 
-    if moves_completed ==10
-      break
-    end
+#   loop do 
+#     if moves_completed ==10
+#       break
+#     end
     
-  puts "To the right, to the right"
-  puts "To the left, to the left"
-  puts "Now kick, kick"
-  puts "Now walk it out"
+#   puts "To the right, to the right"
+#   puts "To the left, to the left"
+#   puts "Now kick, kick"
+#   puts "Now walk it out"
   
-  moves_completed += 1
-  puts "#{moves_completed} move(s) completed!"
-end
+#   moves_completed += 1
+#   puts "#{moves_completed} move(s) completed!"
+# end
 
-end
-dance
+# end
+# dance
